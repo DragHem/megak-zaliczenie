bfb7cb7fcc9265f8cc4e25f17c597cb1b545024a
Damian Puś <pusiek99@interia.pl>
2023-03-16 15:48:42 +0100

    Added manifest link in to app.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,18 @@
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <>
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </>
   );
 }
