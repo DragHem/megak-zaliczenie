140fd5e4a5003f671a7a23a4c020eb640416ac2e
Damian Puś <pusiek99@interia.pl>
2023-03-25 15:58:31 +0100

    Added new icon.

--- services/userService.ts
+++ services/userService.ts
@@ -1,71 +1,65 @@
 import client from "../lib/prismadb";
 
-export abstract class UserService{
+export abstract class UserService {
+  public static async getUser(email: string) {
+    return await client.user.findUnique({
+      where: { email },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        image: true,
+        nickname: true,
+        kittys: true,
+        friends: true,
+      },
+    });
+  }
 
-    public static async getUser(email:string)
-    {
-        return await client.user.findUnique({
-            where:{email},
-            select:{
-                id:true,
-                name:true,
-                email:true,
-                image:true,
-                nickname:true,
-                kittys:true,
-                friends:true,
-            }
-        })
-    }
+  public static async getUserFriends(ids: string[]) {
+    return await client.user.findMany({
+      where: {
+        id: {
+          in: ids,
+        },
+      },
+      select: {
+        id: true,
+        name: true,
+        nickname: true,
+      },
+    });
+  }
 
-    public static async getUserFriends(ids:string[])
-    {
-        return await client.user.findMany({
-            where:{
-                id:{
-                    in:ids
-                }
-            },
-            select:{
-                id:true,
-                name:true,
-                nickname:true,
-            }
-        })
-    }
+  public static async createUser(
+    name: string,
+    email: string,
+    password: string,
+    nickname: string
+  ) {
+    //@toDo dodać walidację
 
-    public static async createUser(
-        name:string,
-        email:string,
-        password:string,
-        nickname:string,
-        ){
-        //@toDo dodać walidację
+    return await client.user.create({
+      data: {
+        name,
+        email,
+        password,
+        nickname,
+      },
+    });
+  }
 
-        return await client.user.create({
-            data:{
-                name,
-                email,
-                password,
-                nickname,
-            }
-        })
-    }
-
-    public static async updateUser(
-        email:string,
-        name:string,
-        nickname:string,
-    )
-    {
-        return await client.user.update({
-            where:{email},
-            data:{
-                name,
-                nickname
-            }
-        })
-    }
-
-
+  public static async updateUser(
+    email: string,
+    name: string,
+    nickname: string
+  ) {
+    return await client.user.update({
+      where: { email },
+      data: {
+        name,
+        nickname,
+      },
+    });
+  }
 }
