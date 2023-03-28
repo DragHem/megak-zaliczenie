e657af24af981b1b004f62b48d45cb7999c5d6b6
Damian Puś <pusiek99@interia.pl>
2023-03-28 20:11:00 +0200

    Added extractBodyFromFormData method in CloudinaryModule

--- lib/cloudinaryModule.ts
+++ lib/cloudinaryModule.ts
@@ -1,4 +1,10 @@
+import { NextApiRequest } from "next";
+
+import path from "path";
+import fs from "fs";
+
 import { v2 as cloudinary } from "cloudinary";
+import formidable from "formidable";
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -14,7 +20,7 @@ export abstract class PhotoUpload {
     folder: "KittyProject",
   };
 
-  static uploadImage = async (imagePath: string) => {
+  static async uploadImage(imagePath: string) {
     try {
       return await cloudinary.uploader.upload(
         imagePath,
@@ -23,5 +29,26 @@ export abstract class PhotoUpload {
     } catch (e) {
       console.log(e);
     }
-  };
+  }
+
+  static async extractBodyFromFormData(req: NextApiRequest) {
+    const dir = path.join(__dirname, "..", "/temporaryImages");
+
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir);
+    }
+    //@todo doać sprawdzanie typów, chociaż najprostsze
+    return new Promise(function (resolve, reject) {
+      const form = new formidable.IncomingForm({
+        keepExtensions: true,
+        uploadDir: dir,
+        maxFiles: 3,
+      });
+
+      form.parse(req, function (err, fields, files) {
+        if (err) return reject(err);
+        resolve({ fields, files });
+      });
+    });
+  }
 }
